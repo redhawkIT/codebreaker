30cad8f0baf5d10c8b25f47a7daff9050bae5639
redhawkIT <khodkodmitry10011983@hotmail.com>
2017-04-04 17:35:54 -0700

    Moderate progress on main and summary.go

--- apiserver/handlers/summary.go
+++ apiserver/handlers/summary.go
@@ -1,7 +1,9 @@
 package handlers
 
 import (
+	"log"
 	"net/http"
+	"strings"
 )
 
 //openGraphPrefix is the prefix used for Open Graph meta properties
@@ -13,19 +15,31 @@ type openGraphProps map[string]string
 func getPageSummary(url string) (openGraphProps, error) {
 	//Get the URL
 	//If there was an error, return it
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatalf("Error fetching URL: %v\n", err)
+	}
 
 	//ensure that the response body stream is closed eventually
 	//HINTS: https://gobyexample.com/defer
 	//https://golang.org/pkg/net/http/#Response
+	defer response.Body.Close() //  Ensures closure  of response
 
 	//if the response StatusCode is >= 400
 	//return an error, using the response's .Status
 	//property as the error message
+	if response.StatusCode != http.StatusOK {
+		log.Fatalf("Response status code: %d\n", response.StatusCode)
+	}
 
 	//if the response's Content-Type header does not
 	//start with "text/html", return an error noting
 	//what the content type was and that you were
 	//expecting HTML
+	ctype := response.Header.Get("Content-Type")
+	if !strings.HasPrefix(ctype, "text/html") {
+		log.Fatalf("Response was not text/html, instead %s\n", ctype)
+	}
 
 	//create a new openGraphProps map instance to hold
 	//the Open Graph properties you find
@@ -54,28 +68,46 @@ func SummaryHandler(w http.ResponseWriter, r *http.Request) {
 	//   Access-Control-Allow-Origin: *
 	//this will allow JavaScript served from other origins
 	//to call this API
+	w.Header().Add("Access-Control-Allow-Origin", "*")
 
 	//get the `url` query string parameter
 	//if you use r.FormValue() it will also handle cases where
 	//the client did POST with `url` as a form field
 	//HINT: https://golang.org/pkg/net/http/#Request.FormValue
+	url := r.FormValue("url")
+	// name := r.URL.Query().Get("name")
 
 	//if no `url` parameter was provided, respond with
 	//an http.StatusBadRequest error and return
 	//HINT: https://golang.org/pkg/net/http/#Error
+	if len(url) == 0 {
+		http.Error(w, http.StatusText(http.StatusBadRequest),
+			http.StatusBadRequest)
+		// url = "World"
+	}
 
 	//call getPageSummary() passing the requested URL
 	//and holding on to the returned openGraphProps map
 	//(see type definition above)
+	//	TODO: Use openGraphProps type
+	// summary, err := getPageSummary(url)
 
 	//if you get back an error, respond to the client
 	//with that error and an http.StatusBadRequest code
+	// if err != nil {
+	// 	http.Error(w, http.StatusText(http.StatusBadRequest),
+	// 		http.StatusBadRequest)
+	// 	// url = "World"
+	// }
+	// fmt.Printf(summary) //Placeholder for build
 
 	//otherwise, respond by writing the openGrahProps
 	//map as a JSON-encoded object
+
 	//add the following headers to the response before
 	//you write the JSON-encoded object:
 	//   Content-Type: application/json; charset=utf-8
 	//this tells the client that you are sending it JSON
+	w.Header().Add("Content-Type", "application/json; charset=utf-8")
 
 }
--- apiserver/main.go
+++ apiserver/main.go
@@ -1,6 +1,21 @@
 package main
 
-const defaultPort = "80"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/info344-s17/challenges-RcKeller/apiserver/handlers"
+)
+
+// const defaultPort = "80"
+const defaultPort = "8080"
+
+//localhost:8080
+
+//	Sample query using port 4000:
+//	http://localhost:4000/v1/summary?url=http://ogp.me/ (Links to an external site.)
 
 const (
 	apiRoot    = "/v1/"
@@ -12,17 +27,29 @@ func main() {
 	//read and use the following environment variables
 	//when initializing and starting your web server
 	// PORT - port number to listen on for HTTP requests (if not set, use defaultPort)
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = defaultPort
+		fmt.Printf("PORT not defined in environment, defaulting to " + defaultPort + "\n")
+	}
+	port = "localhost:" + port
 	// HOST - host address to respond to (if not set, leave empty, which means any host)
+	host := os.Getenv("HOST")
+	if len(host) == 0 {
+		host = ""
+		fmt.Printf("HOST not defined in environment, defaulting to none\n")
+	}
 
 	//add your handlers.SummaryHandler function as a handler
 	//for the apiSummary route
 	//HINT: https://golang.org/pkg/net/http/#HandleFunc
+	http.HandleFunc(apiSummary, handlers.SummaryHandler)
+
+	fmt.Printf("Server is live at %s...\n", port)
 
 	//start your web server and use log.Fatal() to log
 	//any errors that occur if the server can't start
 	//HINT: https://golang.org/pkg/net/http/#ListenAndServe
+	log.Fatal(http.ListenAndServe(port, nil))
 
 }
-
-//	New line for pr.
-//	Line 2
