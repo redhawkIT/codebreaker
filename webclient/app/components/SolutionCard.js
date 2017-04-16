50b9f90a5c2c8c31760307c372a9667f9420c7b9
redhawkIT <khodkodmitry10011983@hotmail.com>
2017-04-16 13:00:38 -0700

    Glitch in table rendering due to material-ui weirdness.

--- webclient/app/components/SolutionCard.js
+++ webclient/app/components/SolutionCard.js
@@ -5,10 +5,26 @@ import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColu
 
 const SolutionCard = ({source, solutions}) => (
   <Card>
-    <CardHeader title='Casar Transposition Solutions'
+    <CardHeader title='Transposition Solutions'
       subtitle={`Query: ${source}`}
     />
     <CardText>{solutions[13]}</CardText>
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHeaderColumn>Key</TableHeaderColumn>
+          <TableHeaderColumn>Solution</TableHeaderColumn>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {Object.keys(solutions).forEach((key) => (
+          <TableRow>
+            <TableRowColumn>Key: {key}</TableRowColumn>
+            <TableRowColumn>Solution: {solutions[key] && console.log(solutions[key])}</TableRowColumn>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
   </Card>
 )
 export default SolutionCard
